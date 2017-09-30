182d4bba70879a4263080a1ec16dd2cb5371cd20
Tom MacKenzie <tom.s.mackenzie@gmail.com>
2017-09-30 08:30:37 -0500

    travis: script to start kafka.

---
+++ run_kafka.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+nohup bash -c "$KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties &"
+nohup bash -c "$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties &"
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test
+
+
